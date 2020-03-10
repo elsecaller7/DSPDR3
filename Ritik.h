6ff5c6c28566c85e2decf111e32514bad7d1ce18
elsecaller7 <60236301+elsecaller7@users.noreply.github.com>
2020-03-10 22:03:33 +0530

    Added status and dll

--- Ritik.h
+++ Ritik.h
@@ -1,7 +1,3 @@
-//
-// Created by ritik on 04/03/20.
-//
-
 #ifndef DSPDR3_RITIK_H
 #define DSPDR3_RITIK_H
 
@@ -10,13 +6,13 @@
 
 #include "datastructs.h"
 void init_ledger(ledger *pq);
-void add_rat(ledger *pq,int cost);
-void add_node(rat *block, int x,int y);
+status add_rat(ledger *pq,int cost);
+status add_node(rat *block, int x,int y);
 int travel(ledger *pq, int i,int max);
 int choose_action(int action[],rat *jerry);
 int is_possible(node *path,int x,int y,int cost);
-void delete_rat(ledger *pq,rat *dead);
-void remove_rat(ledger *pq,rat *dead);
+status delete_rat(ledger *pq,rat *dead);
+status remove_rat(ledger *pq,rat *dead);
 int take_action(rat *jerry,int choice);//return new cost
 rat *split(rat *mouse);
 void move_rat(ledger *pq,rat *jerry, int new_cost);
@@ -25,6 +21,7 @@ void path_print(node *head,FILE *fptr);
 void read_inp();
 void take_maze_input();
 void disp_path();
+void critical_path_print();
 
 
 #endif //DSPDR3_RITIK_H
